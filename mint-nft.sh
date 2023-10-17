e1b1cf5ce3701d9eec40ba6033e1ca361ecf153d
derekchan <chendachao@addcn.com>
2023-10-17 23:14:40 +0800

    update mint script

--- mint-nft.sh
+++ mint-nft.sh
@@ -14,20 +14,23 @@ mint() {
   fi;
 }
 
+run () {
+  wallet_json=$1
+  images_dir=$2
+  gas_fee=${3:-1}
+  bitworkc=${4:-0000}
 
-wallet_json=$1
-images_dir=$2
-gas_fee=${3:-1}
-bitworkc=${4:-0000}
+  # 遍历文件
+  for image in $images_dir/*
+  do
+    filename=$(echo $image | awk -F'/' '{print $NF}')
 
-# 遍历文件
-for image in $images_dir/*
-do
-  filename=$(echo $image | awk -F'/' '{print $NF}')
+    # 调用命令,用$image传递文件名
+    mint "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte="$gas_fee" --funding="funding" --bitworkc="$bitworkc"
 
-  # 调用命令,用$image传递文件名
-  mint "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte="$gas_fee" --funding="funding" --bitworkc="$bitworkc"
+    # 其他处理逻辑
+    echo "Processed image: $image"
+  done
+}
 
-  # 其他处理逻辑
-  echo "Processed image: $image"
-done
+run $@
