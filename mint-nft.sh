865c7a9f73e889835bdd21517edb8a0e8117300a
陈达超 <chendachao@addcn.com>
2023-10-17 15:49:36 +0800

    add gas fee param

--- mint-nft.sh
+++ mint-nft.sh
@@ -3,6 +3,7 @@
 # 文件所在目录
 wallet_json=$1
 images_dir=$2
+gas_fee=${3:-2}
 
 alias atom-cli='f() { if [ -f "$1" ]; then docker run -it --rm -v "$1":/wallet.json -v $2:/app/image  atomicals yarn cli "${@:3}"; else echo "wallet file $1 not exit"; fi; unset -f f; }; f'
 
@@ -12,7 +13,7 @@ do
   filename=$(echo $image | awk -F'/' '{print $NF}')
 
   # 调用命令,用$image传递文件名
-  atom-cli "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte=2 --funding=funding --bitworkc="0000"
+  atom-cli "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte=$gas_fee --funding=funding --bitworkc="0000"
 
   # 其他处理逻辑
   echo "Processed image: $image"
