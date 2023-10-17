9f89398e07112c1d01f13f52fd0c760e74cb6822
derekchan <chendachao@addcn.com>
2023-10-17 23:01:48 +0800

    update mint script

--- mint-nft.sh
+++ mint-nft.sh
@@ -6,7 +6,19 @@ images_dir=$2
 gas_fee=${3:-1}
 bitworkc=${4:-0000}
 
-alias atom-mint='f() { if [ -f "$1" ]; then docker run -it --rm -v "$1":/wallet.json -v $2:/app/image  atomicals yarn cli "${@:3}"; else echo "wallet file $1 not exit"; fi; unset -f f; }; f'
+mint() {
+  wallet=$1
+  image_path=$2
+  shift
+  shift
+
+  if [ -f "$wallet" ];
+  then
+    docker run -it --rm -v "$wallet":/wallet.json -v "$image_path":/app/image atomicals yarn cli "$@";
+  else
+    echo "wallet file $wallet not exit";
+  fi;
+}
 
 # 遍历文件
 for image in $images_dir/*
@@ -14,7 +26,7 @@ do
   filename=$(echo $image | awk -F'/' '{print $NF}')
 
   # 调用命令,用$image传递文件名
-  atom-mint "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte=$gas_fee --funding=funding --bitworkc="$bitworkc"
+  mint "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte="$gas_fee" --funding="funding" --bitworkc="$bitworkc"
 
   # 其他处理逻辑
   echo "Processed image: $image"
