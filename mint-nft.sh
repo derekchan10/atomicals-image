7e89b6274c5650518fc788c7d042d71c144793c9
陈达超 <chendachao@addcn.com>
2023-10-17 15:50:53 +0800

    add bitworkc param

--- mint-nft.sh
+++ mint-nft.sh
@@ -4,6 +4,7 @@
 wallet_json=$1
 images_dir=$2
 gas_fee=${3:-2}
+bitworkc=${4:-0000}
 
 alias atom-cli='f() { if [ -f "$1" ]; then docker run -it --rm -v "$1":/wallet.json -v $2:/app/image  atomicals yarn cli "${@:3}"; else echo "wallet file $1 not exit"; fi; unset -f f; }; f'
 
@@ -13,7 +14,7 @@ do
   filename=$(echo $image | awk -F'/' '{print $NF}')
 
   # 调用命令,用$image传递文件名
-  atom-cli "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte=$gas_fee --funding=funding --bitworkc="0000"
+  atom-cli "$wallet_json" "$images_dir" mint-nft "image/$filename" --satsbyte=$gas_fee --funding=funding --bitworkc="$bitworkc"
 
   # 其他处理逻辑
   echo "Processed image: $image"
