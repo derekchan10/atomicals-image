57a5ea8bcb5d1b99847292e15e50495d5cc6460d
陈达超 <chendachao@addcn.com>
2023-11-14 12:08:36 +0800

    add dmint.sh

--- mint-dft.sh
+++ mint-dft.sh
@@ -47,7 +47,7 @@ run () {
   echo "gas_fee:${gas_fee}"
 
   # 调用命令,用$image传递文件名
-  mint "$wallet_json" "$daemon" mint-dft --satsbyte="$gas_fee" --funding="funding"
+  mint "$wallet_json" "$daemon" mint-dft $ticket --satsbyte="$gas_fee" --funding="funding"
 
   # 其他处理逻辑
   echo "Processed image: $image"
