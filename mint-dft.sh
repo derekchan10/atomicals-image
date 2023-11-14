21cb38a3cc496bb43d79ecc287337d5cb5c9e52b
陈达超 <chendachao@addcn.com>
2023-11-14 12:07:24 +0800

    add dmint.sh

--- mint-dft.sh
+++ mint-dft.sh
@@ -43,12 +43,11 @@ run () {
 
   echo "wallet:${wallet_json},image dir:${images_dir},container:${container},bitworkc:${bitworkc},satsoutput:${satsoutput},daemon=${daemon}"
 
-
   gas_fee=$(gas_fee)
   echo "gas_fee:${gas_fee}"
 
   # 调用命令,用$image传递文件名
-  mint "$wallet_json" "$daemon" mint-dfp --satsbyte="$gas_fee" --funding="funding"
+  mint "$wallet_json" "$daemon" mint-dft --satsbyte="$gas_fee" --funding="funding"
 
   # 其他处理逻辑
   echo "Processed image: $image"
