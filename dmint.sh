9e5597e5ce82e29a7c7b5f8f33f6b109dc6a886b
陈达超 <chendachao@addcn.com>
2023-11-29 13:10:52 +0800

    update

--- dmint.sh
+++ dmint.sh
@@ -5,23 +5,25 @@ source ./cli.sh
 # 运行
 run () {
   wallet_json=$1
-  images_dir=$2
+  dir=$2
   container=$3
-  bitworkc=${4:-1000}
+  daemon=${4:-1}
   satsoutput=${5:-546}
-  daemon=${6:-1}
 
-  echo "wallet:${wallet_json},image dir:${images_dir},container:${container},bitworkc:${bitworkc},satsoutput:${satsoutput},daemon=${daemon}"
+  echo "wallet:${wallet_json},dir:${dir},container:${container},satsoutput:${satsoutput},daemon=${daemon}"
 
   gas_fee=$(gas_fee)
-  echo "gas_fee:"$gas_fee
+  echo "gas_fee:$gas_fee"
 
   # 随机获取图片的编号
-  filename=$(random_file $images_dir)
-  echo "file:"$filename
+  filename=$(random_file $dir)
+  echo "file:$filename"
+
+  item=$(basename "$filename" | sed 's/\.[^.]*$//')
+  echo "item:$item"
 
   # 调用命令,用$image传递文件名
-  mint "$wallet_json" "$images_dir" "$daemon" mint-dmitem $container "$bitworkc"  "image/$filename" --satsbyte="$gas_fee" --funding="funding" --satsoutput="$satsoutput"
+  mint "$wallet_json"  "$dir" "$daemon" mint-item "$container" "$item" "image/$filename" --satsbyte="$gas_fee" --funding="funding" --satsoutput="$satsoutput"
 
   # 其他处理逻辑
   echo "Processed image: $image"
