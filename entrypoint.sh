d84122ef36163940caf1e527462520695b90b97b
陈达超 <chendachao@addcn.com>
2023-12-04 14:46:14 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ cd /app
 
 # check if /wallet.json exists and has content
 if [ -s "/wallet.json" ]; then
-    cp /wallet.json /app/wallet.json
+  mkdir /app/wallets
+  cp /wallet.json /app/wallets/wallet.json
 else
   yarn cli wallet-init
-  cp /app/wallet.json /wallet.json
+  cp /app/wallets/wallet.json /wallet.json
 fi
 
 exec "$@"
