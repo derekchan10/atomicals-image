8782589f66541659305fca5cba90311d602881e8
陈达超 <chendachao@addcn.com>
2023-12-24 14:11:12 +0800

    update dmint

--- mint-dft.sh
+++ mint-dft.sh
@@ -15,8 +15,7 @@ run () {
   # 调用命令,用$image传递文件名
   mint "$wallet_json" "$daemon" mint-dft $ticket --satsbyte="$gas_fee" --funding="funding" --disablechalk
 
-  # 其他处理逻辑
-  echo "Processed image: $image"
+  return 1
 }
 
 source ./start.sh
