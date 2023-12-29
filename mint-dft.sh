40c68a59be6436c43ad3ab31db0241419458034d
陈达超 <chendachao@addcn.com>
2023-12-29 22:13:47 +0800

    update mint-dft supper gas limit

--- mint-dft.sh
+++ mint-dft.sh
@@ -8,23 +8,31 @@ run () {
   daemon=${3:-1}
   gas_offset=${4:-0}
   gas_limit=${5:-0}
-  funding=${6:-"funding"}
+  gas_type=${6:-1}
 
   gas_fee=$(gas_fee)
+  echo "gas_fee:$gas_fee"
 
   if [ $gas_limit -gt 0 ]; then
-    if [ $gas_fee -ge $gas_limit ]; then
-      echo "gas too higher"
-      sleep 3
-      return 0
+    if [ "$gas_type" == "2" ]; then
+      if [ $gas_fee -ge $gas_limit ]; then
+        gas_fee=$gas_limit
+      fi
+    else
+      if [ $gas_fee -ge $gas_limit ]; then
+        echo "gas too higher"
+        sleep 3
+        return 0
+      fi
+
     fi
+
   fi
 
   gas_fee=$((gas_fee + gas_offset))
-  echo "gas_fee:$gas_fee"
 
   # 调用命令,用$image传递文件名
-  mint "$wallet_json" "$daemon" mint-dft $ticket --satsbyte="$gas_fee" --funding="$funding" --disablechalk
+  mint "$wallet_json" "$daemon" mint-dft $ticket --satsbyte="$gas_fee" --funding="funding" --disablechalk
 
   return 1
 }
