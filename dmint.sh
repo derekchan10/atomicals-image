e670fab734b895f4de5b90d023cce68456fc3e06
陈达超 <chendachao@addcn.com>
2024-01-12 19:10:33 +0800

    update node

--- dmint.sh
+++ dmint.sh
@@ -9,11 +9,33 @@ run () {
   container=$3
   daemon=${4:-1}
   gas_offset=${5:-0}
-  satsoutput=${6:-546}
+  gas_limit=${6:-0}
+  gas_type=${7:-1}
+  satsoutput=${8:-546}
 
   echo "wallet:${wallet_json},dir:${dir},container:${container},satsoutput:${satsoutput},daemon=${daemon}"
 
   gas_fee=$(gas_fee)
+  echo "gas_fee:$gas_fee"
+
+  if [ $gas_limit -gt 0 ]; then
+    if [ "$gas_type" == "2" ]; then
+      if [ $gas_fee -ge $gas_limit ]; then
+        gas_fee=$gas_limit
+      fi
+    elif [ "$gas_type" == "3" ]; then
+      gas_fee=$gas_limit
+    else
+      if [ $gas_fee -ge $gas_limit ]; then
+        echo "gas too higher"
+        sleep 3
+        return 0
+      fi
+
+    fi
+
+  fi
+
   gas_fee=$((gas_fee + gas_offset))
   echo "gas_fee:$gas_fee"
 
